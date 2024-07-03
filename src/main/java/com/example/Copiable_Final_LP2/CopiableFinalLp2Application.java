88ac0af295e002691805b9b6cca3827c76e9da9e
Danielito games <scalleyovera@gmail.com>
2024-07-03 18:39:28 -0500

    creacion con spring initialzr

---
+++ src/main/java/com/example/Copiable_Final_LP2/CopiableFinalLp2Application.java
@@ -0,0 +1,13 @@
+package com.example.Copiable_Final_LP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopiableFinalLp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopiableFinalLp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Copiable_Final_LP2/CopiableFinalLp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Copiable_Final_LP2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopiableFinalLp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
